ca245ff60a72922aa0f2b55ac6fc4711297b4875
smforuzmand <career.forouzmand@gmail.com>
2022-10-22 10:33:18 +0200

    -OPEN API - Adding dependency - Adding swagger in properties - Updating the model mapper

--- src/main/java/se/mohsen/restfulllecture_ex/config/AppConfig.java
+++ src/main/java/se/mohsen/restfulllecture_ex/config/AppConfig.java
@@ -2,10 +2,14 @@ package se.mohsen.restfulllecture_ex.config;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+@OpenAPIDefinition(info= @Info(title = "REST API lecture exe" , version = "0.1" ,description = "replicating whatever has been done"))
 @Configuration
 public class AppConfig {
 
